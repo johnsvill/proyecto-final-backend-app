872d5e7fc583497e4b2a8ef976081a5d73d37a3d
Jonathan Villeda <johnsvill@hotmail.com>
2019-05-20 17:51:37 -0600

    Agregando clase en Entities

---
+++ Entities/Inventario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppAlmacenPF.Entities
+{
+    public class Inventario
+    {
+        public int Codigo_Inventario { get; set; }
+        public int Codigo_Producto { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Tipo_Registro { get; set; }
+        public decimal Precio { get; set; }
+        public int Entradas { get; set; }
+        public int Salidas { get; set; }
+    }
+}
